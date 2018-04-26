4df191baa4986ad6528299bac5fb662a935f6820
Alexandre <alexandre_0151@outlook.com>
2018-04-25 23:09:53 -0300

    email.php

---
+++ email.php/index.php
@@ -0,0 +1,27 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf8">
+        <title>Contato</title>
+    </head>
+    <body>
+        <form action="mail_send.php" method="post">
+            <fieldset>
+                <center><label for="email">Seu E-mail</label></center>
+                <center><input required name="email" type="email"></center>
+            </fieldset>
+            <fieldset>
+                <center><label for="mensagem">Horario da Disponibilidade</label></center>
+                <center><textarea required name="Horario da Disponibilidade"></textarea></center>
+            </fieldset>
+            <center><fieldset>WhatsApp</label></center>
+                <center><textarea required name="WhatsApp"></textarea></center>
+            </fieldset>
+            <center><fieldset>Descriçãodo problema com o seu Descktop</label></center>
+                <center><textarea required name="Descriçãodo problema com o seu Descktop"></textarea></center>
+            </fieldset>
+            <fieldset>
+                <center><button type="submit">Agenda Serviço</button></center>
+            </fieldset>
+        </form>
+    </body>
+</html>
